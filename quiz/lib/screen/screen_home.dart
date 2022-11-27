461b73f4a52f393113b0c1fa123f2c6e96d20735
wnsdnn <a24369029@gmail.com>
2022-11-27 17:25:20 +0900

    메인 페이지 퍼블리싱

---
+++ quiz/lib/screen/screen_home.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Quiz App'),
+          backgroundColor: Colors.deepPurple,
+          leading: Container(),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/quiz.jpg',
+                width: width * 0.8,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+            ),
+            Text(
+              "플러터 퀴즈 앱",
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+            ),
+            Text(
+              "퀴즈를 풀기 전 안내사항입니다. \n 꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.",
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+            ),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤 \n 다음 문제를 눌러주세요.'),
+            _buildStep(width, '3. 만점을 향해 도전해보세요!.'),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple,
+                    ),
+                    child: Text(
+                      '지금 퀴즈 풀기',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048,
+        width * 0.048,
+        width * 0.048,
+        width * 0.048,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(title),
+        ],
+      ),
+      // child: Row {
+      //   CrossAxisAlignment: CrossAxisAlignment.start,
+      //   children: <Widget>[
+      //     Icon(
+      //       Icons.check_box,
+      //       size: width * 0.04,
+      //     ),
+      //     Padding(
+      //       padding: EdgeInsets.only(right: width * 0.024),
+      //     ),
+      //   ]
+      // },
+    );
+  }
+}
