e85e794141d2dcd2a3c84b6ddbdc5e1f27ab4c58
wnsdnn <a24369029@gmail.com>
2022-12-07 21:48:04 +0900

    프론트 완성

--- quiz/lib/screen/screen_home.dart
+++ quiz/lib/screen/screen_home.dart
@@ -32,82 +32,85 @@ class _HomeScreenState extends State<HomeScreen> {
     double width = screenSize.width;
     double height = screenSize.height;
 
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('My Quiz App'),
-          backgroundColor: Colors.deepPurple,
-          leading: Container(),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Image.asset(
-                'images/quiz.jpg',
-                // width: width * 0.8,
-                height: height * 0.35,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('My Quiz App'),
+            backgroundColor: Colors.deepPurple,
+            leading: Container(),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Image.asset(
+                  'images/quiz.jpg',
+                  // width: width * 0.8,
+                  height: height * 0.35,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
-            ),
-            Text(
-              "플러터 퀴즈 앱",
-              style: TextStyle(
-                fontSize: width * 0.065,
-                fontWeight: FontWeight.bold,
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
-            ),
-            Text(
-              "퀴즈를 풀기 전 안내사항입니다. \n 꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.",
-              textAlign: TextAlign.center,
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
-            ),
-            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
-            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤 \n 다음 문제를 눌러주세요.'),
-            _buildStep(width, '3. 만점을 향해 도전해보세요!.'),
-            Padding(
-              padding: EdgeInsets.all(width * 0.048),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: width * 0.036),
-              child: Center(
-                child: ButtonTheme(
-                  minWidth: width * 0.8,
-                  height: height * 0.05,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.deepPurple,
+              Text(
+                "플러터 퀴즈 앱",
+                style: TextStyle(
+                  fontSize: width * 0.065,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+              ),
+              Text(
+                "퀴즈를 풀기 전 안내사항입니다. \n 꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.",
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+              ),
+              _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+              _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤 \n 다음 문제를 눌러주세요.'),
+              _buildStep(width, '3. 만점을 향해 도전해보세요!.'),
+              Padding(
+                padding: EdgeInsets.all(width * 0.048),
+              ),
+              Container(
+                padding: EdgeInsets.only(bottom: width * 0.036),
+                child: Center(
+                  child: ButtonTheme(
+                    minWidth: width * 0.8,
+                    height: height * 0.05,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    child: Text(
-                      '지금 퀴즈 풀기',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => QuizScreen(
-                            quizs: quizs,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple,
+                      ),
+                      child: Text(
+                        '지금 퀴즈 풀기',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => QuizScreen(
+                              quizs: quizs,
+                            ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- quiz/lib/screen/screen_quiz.dart
+++ quiz/lib/screen/screen_quiz.dart
@@ -4,6 +4,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:quiz/model/model_quiz.dart';
+import 'package:quiz/screen/screen_result.dart';
 import 'package:quiz/widget/widget_candidate.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -111,6 +112,13 @@ class _QuizScreenState extends State<QuizScreen> {
                       ? null
                       : () {
                           if (_currentIndex == widget.quizs.length - 1) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ResultScreen(
+                                    answers: _answers, quizs: widget.quizs),
+                              ),
+                            );
                           } else {
                             _answerState = [false, false, false, false];
                             _currentIndex += 1;
